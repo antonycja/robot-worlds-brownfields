f292d32870be21d5339f96b30da0577da454b147
Nkosikhona Mlaba <nkosimlaba397@gmail.com>
2024-05-14 15:55:08 +0200

    server-client

--- robot_worlds_13/src/client/Client.java
+++ robot_worlds_13/src/client/Client.java
@@ -4,6 +4,60 @@ package client;
  * This class acts as the user's interface to the program
  * it will employ charecteristics from play
  */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
-    
+    static String address = "localhost";
+    static Socket sThisClient;
+    static DataOutputStream dout;
+    static DataInputStream din;
+    static Scanner line = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        try {
+            sThisClient = new Socket(address, 2201);
+            dout = new DataOutputStream(sThisClient.getOutputStream());
+            din = new DataInputStream(sThisClient.getInputStream());
+
+            while (true) {
+                // get server messages
+                String response = din.readUTF();
+                
+                // print message to this client
+                System.out.println(response);
+                
+                if (response.contains("Shutting down")) {
+                    break;
+                }
+
+                // if the message requires a response prompt current client for input
+                // and send the command
+                // (what do you want to name your robot / what do you want to do next)
+                if (response.startsWith("What")) {
+                    String command = line.nextLine();
+                    dout.writeUTF(command);
+                    dout.flush();
+                }
+            }
+            
+            dout.flush();
+            dout.close();
+            sThisClient.close();
+        }
+        catch (Exception e) {
+            try {
+                
+                dout.writeUTF("off");
+                dout.flush();
+                dout.close();
+                sThisClient.close();
+            } catch (Exception i) {
+                System.out.println("Connection problem to the server encountered");
+            }
+        }
+    }
 }
--- robot_worlds_13/src/server/Server.java
+++ robot_worlds_13/src/server/Server.java
@@ -1,18 +1,8 @@
 package server;
 
-import server.robot.maze.SimpleMaze;
-import server.robot.Command;
-import server.robot.Position;
-import server.robot.Robot;
-import server.robot.world.AbstractWorld;
-import server.robot.world.TextWorld;
-
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
-import java.io.IOException;
+
 
 public class Server {
     public static void main(String[] args) throws Exception {
@@ -23,11 +13,11 @@ public class Server {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Incoming connection accepted");
 
-            
+            // this server
             Server serverObject = new Server();
 
             // Create a new thread for each client
-            ClientHandler clientHandler = new ClientHandler();
+            ClientHandler clientHandler = new ClientHandler(clientSocket);
             Thread clientThread = new Thread(clientHandler);
             clientThread.start();
             }
