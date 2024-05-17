d323ce63bfa0ff6cace8c73a8d86cfde40ac64ef
Summer Ngcobo <sngcobo023@student.wethinkcode.co.za>
2024-05-17 12:39:03 +0200

    updated welcome

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/client/Client.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/client/Client.java
@@ -30,6 +30,16 @@ public class Client {
                 "      _)(  )(_\n" +
                 "     `---''---`");
 
+        System.out.println("Here is a list of things I can do:\n"+
+                "OFF  - Shut down robot\n" +
+//                "HELP - provide information about commands\n" +
+                "FORWARD - move forward by specified number of steps, e.g. 'FORWARD 10'\n" +
+                "BACK - move back by specified number of steps, e.g. 'BACK 10'\n" +
+                "LEFT - turn left by 90 degrees\n"+
+                "RIGHT - turn right by 90 degrees\n");
+
+
+
         try {
             sThisClient = new Socket(serverIpAddress, 2201);
         } catch (Exception ex) {
