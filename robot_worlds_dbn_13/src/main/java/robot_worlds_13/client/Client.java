6d093cb786fac07c382f33f5978ff4d676a3f266
Summer Ngcobo <sngcobo023@student.wethinkcode.co.za>
2024-05-20 08:17:03 +0200

    server-client

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/client/Client.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/client/Client.java
@@ -1,12 +1,7 @@
 package robot_worlds_13.client;
 
-/*
- * This class acts as the user's interface to the program
- * it will employ charecteristics from play
- */
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -29,24 +24,21 @@ public class Client {
                 "      (--/\\--)    \\__/\n" +
                 "      _)(  )(_\n" +
                 "     `---''---`");
-
         System.out.println("Here is a list of things I can do:\n"+
                 "OFF  - Shut down robot\n" +
-//                "HELP - provide information about commands\n" +
                 "FORWARD - move forward by specified number of steps, e.g. 'FORWARD 10'\n" +
                 "BACK - move back by specified number of steps, e.g. 'BACK 10'\n" +
                 "LEFT - turn left by 90 degrees\n"+
                 "RIGHT - turn right by 90 degrees\n");
 
 
-
         try {
-            sThisClient = new Socket(serverIpAddress, 2201);
+            sThisClient = new Socket(serverIpAddress, 2204);
         } catch (Exception ex) {
             System.out.println("Failed to connect to the server using the specified IP address.");
             System.out.println("Resorting to localhost...\n");
             try {
-                sThisClient = new Socket(localAddress, 2201);
+                sThisClient = new Socket(localAddress, 2204);
             } catch (Exception e) {
                 System.out.println("Failed to connect to localhost.");
                 System.out.println("None of the known servers are running");
@@ -60,21 +52,20 @@ public class Client {
             din = new DataInputStream(sThisClient.getInputStream());
 
             while (true) {
-                // get server messages
+                // Get server messages
                 String response = din.readUTF();
-                
-                // print message to this client
+
+                // Print message to this client
                 System.out.println(response);
-                
+
                 if (response.contains("Shutting down")) {
                     break;
                 } else if (response.contains("Server shutting down...")) {
                     break;
                 }
-                
-                // if the message requires a response prompt current client for input
+
+                // If the message requires a response prompt current client for input
                 // and send the command
-                // (what do you want to name your robot / what do you want to do next)
                 if (response.startsWith("What")) {
                     String command = line.nextLine();
                     dout.writeUTF(command);
@@ -82,17 +73,15 @@ public class Client {
                 }
             }
 
-            // close this client
+            // Close this client
             dout.flush();
             dout.close();
         }
         catch (Exception e) {
             try {
-                
                 dout.writeUTF("off");
                 dout.flush();
                 dout.close();
-                // sThisClient.close();
             } catch (Exception i) {
                 System.out.println("Connection problem encountered: Server is down or you do not an active internet connection");
             }
--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/Server.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/Server.java
@@ -1,40 +1,43 @@
 package robot_worlds_13.server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Server {
-    List<ClientHandler> clients = new ArrayList<>();
+    static List<ClientHandler> clients = new ArrayList<>();
+
+    public static void broadcast(String message) {
+        for (ClientHandler client : clients) {
+            try {
+                client.sendMessage(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        
-        // this server
         Server serverObject = new Server();
 
-        try (ServerSocket serverSocket = new ServerSocket(2201)) {
+        try (ServerSocket serverSocket = new ServerSocket(2204)) {
             System.out.println("Server started. Listening for incoming connections...");
 
-            // thread for listening to terminal commands
-            Thread terminalListenerThread = new Thread(new TerminalListener(serverSocket, serverObject));
-            terminalListenerThread.start();;
-
             while (true) {
-                // accepting clients
+                // Accepting clients
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Incoming connection accepted");
 
                 // Create a new thread for each client
                 ClientHandler clientHandler = new ClientHandler(clientSocket);
-                serverObject.clients.add(clientHandler);
+                clients.add(clientHandler); // Add client handler to the list of clients
                 Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
-        }
-        catch (Exception e) {
-            System.out.println("Server closed");
+        } catch (Exception e) {
+            System.out.println("Server closed due to error: " + e.getMessage());
         }
     }
 }
