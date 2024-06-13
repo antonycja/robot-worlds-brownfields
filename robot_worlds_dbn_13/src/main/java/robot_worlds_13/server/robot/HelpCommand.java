b987bddf3d92c40c68af1da89b3afbe1c3e19a3e
tmvuni023 <tmvuni023@student.wethinkcode.co.za>
2024-06-13 13:59:09 +0200

    Added new commands in help

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/HelpCommand.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/HelpCommand.java
@@ -33,7 +33,10 @@ public class HelpCommand extends Command {
                 "BACK - move back by specified number of steps, e.g. 'BACK 10'\n" +
                 "LEFT - turn left by 90 degrees\n"+
                 "RIGHT - turn right by 90 degrees\n" +
-                "OFF  - Shut down robot";
+                "OFF  - Shut down robot\n" +
+                "quit - disconnects all robots and ends the world.\n" +
+                "robots - lists all robots in the world including the robot’s name and state\n" +
+                "dump - displays a representation of the world’s state showing robots, obstacles, and anything else in the world that you programmed";
 
         // Construct response data and state
         Map<String, Object> data = new HashMap<>();
