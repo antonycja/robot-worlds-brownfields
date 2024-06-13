32ab74c22ac817017c2ab456b1c52de870810671
tmvuni023 <tmvuni023@student.wethinkcode.co.za>
2024-06-13 13:18:01 +0200

    Refactored Reload code and added docstrings

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/ReloadCommand.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/ReloadCommand.java
@@ -2,32 +2,52 @@ package robot_worlds_13.server.robot;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import robot_worlds_13.server.ServerProtocol;
 
+/**
+ * Represents a command to reload ammunition for a robot.
+ * When executed, this command simulates the reload time for the robot and replenishes its ammunition.
+ */
 public class ReloadCommand extends Command{
+
+    /**
+     * Constructs a ReloadCommand object.
+     * Initializes the command name as "reload".
+     * @param robot The robot for which the reload command is intended.
+     */
     public ReloadCommand(Robot robot) {
         super("reload"); // Call to Command constructor
     }
 
+    /**
+     * Constructs a ReloadCommand object.
+     * Initializes the command name as "reload".
+     */
     public ReloadCommand() {
         super("reload");
     }
 
+    /**
+     * Executes the reload command for the given target robot.
+     * @param target The robot executing the reload command.
+     * @return true if the command was executed successfully, false otherwise.
+     */
     @Override
     public boolean execute(Robot target) {
-        // System.out.println("Reloading...");
-        // simulate reload time
+        // Simulate reload time and replenish ammunition
         
         int reloadTime = target.getReloadTime();
-
         target.reload(reloadTime);
-        
+
+        // Construct response data and state
         Map<String, Object> data = new HashMap<>();
         data.put("message", "Done");
         Map<String, Object> state = target.getRobotState();
+
+        // Set response to the robot
         target.setResponseToRobot(ServerProtocol.buildResponse("OK", data, state));
 
+        // Set GUI response
         data.clear();
         data.put("message", "NONE");
         state.clear();
