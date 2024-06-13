8f1f613da6551de992f42dcf2c4c5ff2a1b0adc3
tmvuni023 <tmvuni023@student.wethinkcode.co.za>
2024-06-13 13:19:57 +0200

    Refactored Look code and added docstrings

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/LookCommand.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/LookCommand.java
@@ -3,26 +3,39 @@ package robot_worlds_13.server.robot;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
 import robot_worlds_13.server.ServerProtocol;
 
+/**
+ * Represents a command to allow a robot to look around and gather information about its surroundings.
+ * When executed, this command utilizes the abstract world data to gather information about nearby obstacles.
+ * It generates a list of obstructions and provides this information as a response to the robot and GUI.
+ */
 public class LookCommand extends Command{
-    
+
+    /**
+     * Executes the look command for the given target robot.
+     * @param target The robot executing the look command.
+     * @return true if the command was executed successfully, false otherwise.
+     */
     @Override
     public boolean execute(Robot target) {
-        
-        // look using abstract world
-        target.worldData.giveCurrentRobotInfo(target);
-        
 
-        // return list with details
+        // Look around using abstract world data
+        target.worldData.giveCurrentRobotInfo(target);
+
+
+        // Return list with details of obstructions
         ArrayList<Object> obstructions = target.worldData.lookAround();
 
+        // Construct response data and state
         Map<String, Object> data = new HashMap<>();
         data.put("objects", obstructions);
         Map<String, Object> state = target.getRobotState();
+
+        // Set response to the robot
         target.setResponseToRobot(ServerProtocol.buildResponse("OK", data, state));
 
+        // Set GUI response
         data.clear();
         data.put("message", "NONE");
         state.clear();
