f06ac490dfed80b1bb886b56fb00bed2f1e36af6
tmvuni023 <tmvuni023@student.wethinkcode.co.za>
2024-06-13 13:14:38 +0200

    Refactored State code and added docstrings

--- robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/StateCommand.java
+++ robot_worlds_dbn_13/src/main/java/robot_worlds_13/server/robot/StateCommand.java
@@ -2,11 +2,18 @@ package robot_worlds_13.server.robot;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import robot_worlds_13.server.ServerProtocol;
 
+/**
+ * Command to retrieve the state of the robot.
+ */
 public class StateCommand extends Command {
-    
+
+    /**
+     * Executes the state command to retrieve the state of the target robot.
+     * @param target The robot to retrieve the state from.
+     * @return Always returns true to indicate successful execution.
+     */
     @Override
     public boolean execute(Robot target) {
         
@@ -26,6 +33,10 @@ public class StateCommand extends Command {
         return true;
     }
 
+    /**
+     * Initializes the state command with a given argument.
+     * @param argument The argument associated with the state command (unused).
+     */
     public StateCommand (String argument) {
         super("state");
     }
