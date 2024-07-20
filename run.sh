bd544d23f90004d020dd07f4f99ad8b33f905483
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-07-20 12:06:38 +0200

    added different server running options

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# run the default server without any args
 java -jar libs/reference-server-0.1.0.jar
+
+# run server on a different port
+#java -jar libs/reference-server-0.1.0.jar -p 5001
+
+# run server with size of 20x20
+#java -jar libs/reference-server-0.1.0.jar -s 20 -o 1,2
+
+# run server with obstacle at [1,2]
+#java -jar libs/reference-server-0.1.0.jar -o 1,2
+
+# run server on a different port with size of 20x20 and obstacle at [1,2]
+#java -jar libs/reference-server-0.1.0.jar -p 5001 -s 20 -o 1,2
