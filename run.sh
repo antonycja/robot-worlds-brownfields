75c4138fd7bb3b3d54bf8d054e11d18589f36c8e
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-07-22 15:42:09 +0200

    updated run bash script to accept flags/args

--- run.sh
+++ run.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
 
-# run the default server without any args
-java -jar libs/reference-server-0.2.3.jar
+# Function to display help message
+show_help() {
+  echo "Usage: $0 [-h] [-d] [-p] [-s] [-o] [-a]"
+  echo
+  echo "  -h        Display this help message."
+  echo "  -d        Run the default server."
+  echo "  -p        Run the server on a different port. (5001)"
+  echo "  -s        Run the server with a size of 2x2."
+  echo "  -o        Run the server with an obstacle at [1,2]."
+  echo "  -a        Run the server on a different port (5001) with size 2x2 and obstacle at [1,2]."
+}
 
-# run server on a different port
-#java -jar libs/reference-server-0.2.3.jar -p 5001
+# Initialize default command
+cmd="java -jar libs/reference-server-0.2.3.jar"
 
-# run server with size of 20x20
-#java -jar libs/reference-server-0.2.3.jar -s 20 -o 1,2
+# Parse command-line options
+while getopts ":hdpsoca" opt; do
+  case ${opt} in
+    h )
+      show_help
+      exit 0
+      ;;
+    d )
+      cmd="java -jar libs/reference-server-0.2.3.jar"
+      ;;
+    p )
+      cmd="$cmd -p 5001"
+      ;;
+    s )
+      cmd="$cmd -s 2"
+      ;;
+    o )
+      cmd="$cmd -o 1,2"
+      ;;
+    a | c )
+      cmd="$cmd -p 5001 -s 2 -o 1,2"
+      break
+      ;;
+    \? )
+      echo "Invalid option: -$OPTARG" >&2
+      show_help
+      exit 1
+      ;;
+    : )
+      echo "Option -$OPTARG requires an argument." >&2
+      show_help
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
-# run server with obstacle at [1,2]
-#java -jar libs/reference-server-0.2.3.jar -o 1,2
-
-# run server on a different port with size of 20x20 and obstacle at [1,2]
-#java -jar libs/reference-server-0.2.3.jar -p 5001 -s 20 -o 1,2
+# Run the command
+echo "Running command: $cmd"
+$cmd
