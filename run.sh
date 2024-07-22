58e8fd2396c04c0dadfa8f257ade231c2d467c0c
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-07-22 11:44:40 +0200

    updated run script to run the new server with args

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # run the default server without any args
-java -jar libs/reference-server-0.1.0.jar
+java -jar libs/reference-server-0.2.3.jar
 
 # run server on a different port
-#java -jar libs/reference-server-0.1.0.jar -p 5001
+#java -jar libs/reference-server-0.2.3.jar -p 5001
 
 # run server with size of 20x20
-#java -jar libs/reference-server-0.1.0.jar -s 20 -o 1,2
+#java -jar libs/reference-server-0.2.3.jar -s 20 -o 1,2
 
 # run server with obstacle at [1,2]
-#java -jar libs/reference-server-0.1.0.jar -o 1,2
+#java -jar libs/reference-server-0.2.3.jar -o 1,2
 
 # run server on a different port with size of 20x20 and obstacle at [1,2]
-#java -jar libs/reference-server-0.1.0.jar -p 5001 -s 20 -o 1,2
+#java -jar libs/reference-server-0.2.3.jar -p 5001 -s 20 -o 1,2
