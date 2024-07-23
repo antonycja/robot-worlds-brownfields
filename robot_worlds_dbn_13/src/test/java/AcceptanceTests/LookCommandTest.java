3c3d1fdf05000babfc479538208603a8cf7fcbdf
ljonase023 <ljonase023@student.wethinkcode.co.za>
2024-07-23 14:58:31 +0200

    Docker skeleton that I will be working at from home and unfished look tests
    Signed-off-by: ljonase023 <ljonase023@student.wethinkcode.co.za>

--- robot_worlds_dbn_13/src/test/java/AcceptanceTests/LookCommandTest.java
+++ robot_worlds_dbn_13/src/test/java/AcceptanceTests/LookCommandTest.java
@@ -21,11 +21,12 @@ public class LookCommandTest {
 
 
     @BeforeEach
-    void connectToServer(){
+    void connectToServer() {
         serverClient.connect(DEFAULT_IP, DEFAULT_PORT);
     }
+
     @AfterEach
-    void disconnectFromServer(){
+    void disconnectFromServer() {
         serverClient.disconnect();
     }
 
@@ -53,5 +54,53 @@ public class LookCommandTest {
 
         // And I should also get the state of the robot
         assertNotNull(response.get("state"));
+    }
+
+    @Test
+    public void See_robots_and_obstacles() {
+        //Given I launch a robot and in a  world size 2x2
+        assertTrue(serverClient.isConnected());
+
+        // And I have launched a robot into the world
+        serverClient.sendRequest(launchRequest);
+
+        // When I send a valid look request to the server
+        String request = "{" +
+                "  \"robot\": \"HAL\"," +
+                "  \"command\": \"look\"," +
+                "  \"arguments\": [\"shooter\",\"5\",\"5\"]" +
+                "}";
+
+        JsonNode response = serverClient.sendRequest(request);
+
+        // Then I should get a valid response from a server
+        assertNotNull(response.get("result"));
+        assertEquals("OK", response.get("result").asText());
+
+        // And I should also get the state of the robot
+        assertNotNull(response.get("state"));
+
+        // Check for obstacles and other robots
+        JsonNode observation = response.get("state").get("type"); // Adjust path according to your actual JSON
+      /*  and the world has an obstacle at coordinate [0,1]
+        and I have successfully launched 8 robots into the world*/
+        int obstacleCount = 0;
+        int robotCount = 8;
+
+        // Iterate through the observation to count the items
+        for (JsonNode item : observation) {
+            String type = item.get("type").asText();
+            if ("OBSTACLE".equals(type)) {
+                obstacleCount++;
+            } else if ("ROBOT".equals(type)) {
+                robotCount++;
+            }
+
+        }
+       /* Then I should get an response back with
+        one object being an OBSTACLE that is one step away
+        and three objects should be ROBOTs that is one step away*/
+        assertEquals(1, obstacleCount);
+        assertEquals(3, robotCount, response.get("result").asText());
     }
 }
