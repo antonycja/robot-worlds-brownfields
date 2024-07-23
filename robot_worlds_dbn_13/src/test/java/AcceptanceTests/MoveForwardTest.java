b57b261b743cdf3f0c88c7a39ae5c97a42224fa8
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-07-23 11:50:41 +0200

    created test to test robot that does not exist with forward command

--- robot_worlds_dbn_13/src/test/java/AcceptanceTests/MoveForwardTest.java
+++ robot_worlds_dbn_13/src/test/java/AcceptanceTests/MoveForwardTest.java
@@ -28,4 +28,28 @@ public class MoveForwardTest {
         serverClient.disconnect();
     }
 
+    @Test
+    void MovingNoneExistentRobotShouldFail() {
+        // Given that I am connected to a running Robot Worlds server
+        assertTrue(serverClient.isConnected());
+
+        // And a robot called "HAL" is connected and launched
+        serverClient.sendRequest(launchRequest);
+
+        // When I send a command for "TOM" to move forward by 5 steps
+        String request = "{" +
+                "  \"robot\": \"TOM\"," +
+                "  \"command\": \"forward\"," +
+                "  \"arguments\": [\"5\"]" +
+                "}";
+        JsonNode response = serverClient.sendRequest(request);
+
+        // Then I should get an "ERROR" response with the message "Robot does not exist"
+        assertNotNull(response.get("result"));
+        assertEquals("ERROR", response.get("result").asText());
+        assertNotNull(response.get("data"));
+        assertNotNull(response.get("data").get("message"));
+        assertEquals("Robot does not exist", response.get("data").get("message").asText());
+    }
+
 }
