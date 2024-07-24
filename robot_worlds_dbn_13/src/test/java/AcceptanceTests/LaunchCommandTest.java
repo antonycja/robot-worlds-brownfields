c27ac4bc1e435be9d87296439ef2ef810ae96d42
tarendse023 <tarendse023@student.wethinkcode.co.za>
2024-07-24 14:46:34 +0200

    launch finish

--- robot_worlds_dbn_13/src/test/java/AcceptanceTests/LaunchCommandTest.java
+++ robot_worlds_dbn_13/src/test/java/AcceptanceTests/LaunchCommandTest.java
@@ -170,24 +170,22 @@ class LaunchRobotTests {
             assertEquals("OK", response.get("result").asText());
         }
 
-    }
+        // When I launch one more robot
+        String duplicateLaunchRequest = "{" +
+                "  \"robot\": \"HAL10\"," +
+                "  \"command\": \"launch\"," +
+                "  \"arguments\": [1, 1]" +
+                "}";
 
-        {// When I launch one more robot
-            String duplicateLaunchRequest = "{" +
-                    "  \"robot\": \"HAL10\"," +
-                    "  \"command\": \"launch\"," +
-                    "  \"arguments\": [1, 1]" +
-                    "}";
+        JsonNode duplicateLaunchResponse = serverClient.sendRequest(duplicateLaunchRequest);
+        // Then I should get an error response back with the message "No more space in this world"
 
-            JsonNode duplicateLaunchResponse = serverClient.sendRequest(duplicateLaunchRequest);
-            // Then I should get an error response back with the message "No more space in this world"
-
-            assertNotNull(duplicateLaunchResponse.get("result"));
-            assertEquals("ERROR", duplicateLaunchResponse.get("result").asText());
-            assertNotNull(duplicateLaunchResponse.get("data"));
-            assertNotNull(duplicateLaunchResponse.get("data").get("message"));
-            assertEquals("No more space in this world.", duplicateLaunchResponse.get("data").get("message").asText());
-        }
+        assertNotNull(duplicateLaunchResponse.get("result"));
+        assertEquals("ERROR", duplicateLaunchResponse.get("result").asText());
+        assertNotNull(duplicateLaunchResponse.get("data"));
+        assertNotNull(duplicateLaunchResponse.get("data").get("message"));
+        assertEquals("No more space in this world", duplicateLaunchResponse.get("data").get("message").asText());
 
     }
 
+}
