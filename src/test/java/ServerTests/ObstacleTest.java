83f9d673fbb1826c3943683042a7d14030445902
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-08-16 11:51:46 +0200

    updated obstacle test

--- src/test/java/ServerTests/ObstacleTest.java
+++ src/test/java/ServerTests/ObstacleTest.java
@@ -8,9 +8,10 @@ import robot_worlds_13.server.robot.world.SquareObstacle;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ObstacleTest {
+    private int size = 40;
     @Test
     void testObstacleDimensions() {
-        Obstacle obstacle = new SquareObstacle(1,1);
+        Obstacle obstacle = new SquareObstacle(1,1, size);
         assertEquals(1, obstacle.getBottomLeftX());
         assertEquals(1, obstacle.getBottomLeftY());
         assertEquals(40, obstacle.getSize());
@@ -18,7 +19,7 @@ public class ObstacleTest {
 
     @Test
     void testBlockPosition(){
-        Obstacle obstacle = new SquareObstacle(1,1);
+        Obstacle obstacle = new SquareObstacle(1,1, size);
         assertTrue(obstacle.blocksPosition(new Position(1,1)));
         assertTrue(obstacle.blocksPosition(new Position(5,1)));
         assertTrue(obstacle.blocksPosition(new Position(1,5)));
@@ -28,7 +29,7 @@ public class ObstacleTest {
 
     @Test
     void testBlockPath(){
-        Obstacle obstacle = new SquareObstacle(1,1);
+        Obstacle obstacle = new SquareObstacle(1,1, size);
         assertTrue(obstacle.blocksPath(new Position(1,0), new Position(1,50)));
         assertTrue(obstacle.blocksPath(new Position(2,-10), new Position(2, 100)));
         assertTrue(obstacle.blocksPath(new Position(-10,5), new Position(20, 5)));
