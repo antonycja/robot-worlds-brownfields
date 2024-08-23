486a3504b49ee05123c3f103c858a5929669aab0
ljonase023 <ljonase023@student.wethinkcode.co.za>
2024-08-23 16:06:38 +0200

    updated RobotWorldServer

--- src/main/java/webAPI/RobotWorldServer.java
+++ src/main/java/webAPI/RobotWorldServer.java
@@ -2,17 +2,17 @@ package webAPI;
 
 import io.javalin.Javalin;
 
-
 public class RobotWorldServer {
 
     private Javalin server;
 
     public RobotWorldServer() {
         server = Javalin.create();
-        this.server.get("/world", RobotWorldApi::getCurrentWorld);
-        //this.server.get("/world/{world}",RobotWorldApi::restoreSavedWorld);
-    }
 
+        // Ensure that RobotWorldApi methods are correctly referenced
+        this.server.get("/world", RobotWorldApi::getCurrentWorld);
+        this.server.get("/world/{world}", RobotWorldApi::restoreSavedWorld);
+    }
 
     public void start(int port) {
         this.server.start(port);
@@ -24,7 +24,6 @@ public class RobotWorldServer {
 
     public static void main(String[] args) {
         RobotWorldServer server = new RobotWorldServer();
-        server.start(5050);
+        server.start(3000);
     }
-
 }
