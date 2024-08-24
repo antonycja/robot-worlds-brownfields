fa5b9a35d51b406650dcf5588e3b508f05e4cb42
amaposa023 <amaposa023@student.wethinkcode.co.za>
2024-08-24 17:07:00 +0200

    cleaned up restore world and now using orm

--- src/main/java/robot_worlds_13/server/robot/RestoreCommand.java
+++ src/main/java/robot_worlds_13/server/robot/RestoreCommand.java
@@ -9,11 +9,9 @@ import robot_worlds_13.server.robot.world.SquareObstacle;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import static database.orm.ConnectDB.worldDAO;
-import static robot_worlds_13.server.robot.SaveCommand.obstacleTypes;
 
 public class RestoreCommand {
     private final String worldName;
@@ -29,12 +27,10 @@ public class RestoreCommand {
 
     public RestoreCommand(String worldName) {
         this.worldName = (worldName.isEmpty()) ? promptForWorldName() : worldName;
-//        addObstacleTypes();
     }
 
     public RestoreCommand() {
         this.worldName = promptForWorldName();
-//        addObstacleTypes();
     }
 
     private String promptForWorldName() {
@@ -96,61 +92,4 @@ public class RestoreCommand {
 
         } catch (IllegalStateException e) {System.out.println("Restore process terminated: " + e.getMessage());}
     }
-//    public void restoreWorld(AbstractWorld world) {
-//        try {
-//            String worldTableName = "world";
-//            String obstacleTableName = "objects";
-//            String typeTableName = "types";
-//
-//            Map<String, Object> worldData = sqlCommands.restoreWorldData(worldTableName, obstacleTableName, typeTableName, this.worldName);
-//
-//            String worldName = (String) worldData.get("name");
-//            Integer worldWidth = (Integer) worldData.get("width");
-//            Integer worldHeight = (Integer) worldData.get("height");
-//
-//            ArrayList<Object> objects = (ArrayList<Object>) worldData.get("obstacles");
-//            final List<ObjectsDO> worldObjects = worldDAO.getObjectData(worldName);
-//
-//            if (!this.worldName.equals(worldName)) {
-//                System.out.println("The world '" + this.worldName + "' does not exist!!");
-//                System.out.println("Aborting Restore...");
-//                System.out.println("Successfully Reverted back.");
-//            } else {
-//                for (Object object : objects) {
-//                    Map<String, Object> obs = (Map<String, Object>) object;
-//                    int obstacleSize = (Integer) obs.get("size");
-//                    int xPosition = (Integer) obs.get("x_position");
-//                    int yPosition = (Integer) obs.get("y_position");
-//                    String type = (String) obs.get("type");
-//
-//                    // Create obstacles with dynamic size
-//                    Obstacle obstacle = new SquareObstacle(xPosition, yPosition, obstacleSize,type);
-//
-//                    switch (type) {
-//                        case "obstacle":
-//                            obstacles.add(obstacle);
-//                            break;
-//                        case "lake":
-//                            lakes.add(obstacle);
-//                            break;
-//                        case "pit":
-//                            pits.add(obstacle);
-//                            break;
-//                        default:
-//                            System.out.println("Unknown obstacle type: " + type);
-//                            break;
-//                    }
-//                }
-//
-//                world.setObstacles(obstacles);
-//                world.setBottomLessPits(pits);
-//                world.setLakes(lakes);
-//
-//                System.out.println("World restored successfully with the name: " + worldName);
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
